e6eb6aeee7e30f9dbbe28f27609ff3ca88407a2b
unknown <ahmed.hesham.ahmed10@gmail.com>
2024-02-10 17:39:57 +0200

    nav

--- src/component/Applayout/Applayout.jsx
+++ src/component/Applayout/Applayout.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from 'react-router-dom';
 import Search from '../../pages/search/search';
+import Navbar from './NavBar';
 
 
 const AppLayout = () => {
     return (
        <>
+       <Navbar/>
        <Search/>
        <Outlet/>
        </>
